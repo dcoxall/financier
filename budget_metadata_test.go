a0ccf807f97cb104fa289fe194f69c1d494c06d9
Darren Coxall <darren@darrencoxall.com>
2015-01-07 19:47:24 +0000

    This is actually tested elsewhere

--- budget_metadata_test.go
+++ budget_metadata_test.go
@@ -6,17 +6,6 @@ import (
 	"testing"
 )
 
-func TestBudgetMetadataAttributes(t *testing.T) {
-	meta := BudgetMetadata{
-		RelativeDataFolderName: "data2-69747A23",
-		TED:           17199000000000,
-		FormatVersion: "2",
-	}
-	assert.Equal(t, meta.RelativeDataFolderName, "data2-69747A23")
-	assert.Equal(t, meta.TED, 17199000000000)
-	assert.Equal(t, meta.FormatVersion, "2")
-}
-
 func TestBudgetMetadataJSONUnarshal(t *testing.T) {
 	jsonString := `{
 		"relativeDataFolderName": "data2-69747A23",
